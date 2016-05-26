ea78ad466986881107f4ba8ddaf8a378638e6e06
Rails Udemy Course <sebastian.alvarado@mail.utoronto.ca>
2016-05-26 17:41:23 -0500

    Added Edit,Update,Show and Index Listing

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,7 @@
 class ArticlesController < ApplicationController
 	def index
+		#Grab articles from the database
+		@all_articles = Article.all
 	end
 	def new
 		@article = Article.new
@@ -20,6 +22,22 @@ class ArticlesController < ApplicationController
 
 	def show
 			@article = Article.find(params[:id])
+	end
+
+	def edit
+		@article = Article.find(params[:id])
+	end
+
+
+	def update
+		@article = Article.find(params[:id])
+		if @article.update(article_params)
+			flash[:notice] = "Article was successfully updated"
+			redirect_to article_path(@article)
+		else
+			render 'edit'
+		end
+			
 	end
 
 	#Helper method to get the params that were passed
