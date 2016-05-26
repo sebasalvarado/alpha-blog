2829e7ece19c742c03f368e2cecb0b212c257b6b
Rails Udemy Course <sebastian.alvarado@mail.utoronto.ca>
2016-05-26 18:02:43 -0500

    All CRUD actions are represented, just added Delete

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ root to:'pages#home'
 
 get 'about', to:'pages#about'
 
+#Creates the paths for all the CRUD operations on articles
 resources :articles
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
