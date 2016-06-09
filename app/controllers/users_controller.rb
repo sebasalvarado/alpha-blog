cbdce6c4e1412c9b270d0f3700d4849f62865973
Rails Udemy Course <sebastian.alvarado@mail.utoronto.ca>
2016-06-09 11:53:07 -0500

    Added Article listing for user and Gravatar profile image as well

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -30,6 +30,11 @@ class UsersController < ApplicationController
 			render 'edit'
 		end
 	end
+
+	def show
+		#Find the user to show
+		@user = User.find(params[:id])
+	end
 	private 
 	def user_params
 		params.require(:user).permit(:username, :email, :password)
