dab945f4fd100289b19d7ce501ad7a3e8ed629ce
Rails Udemy Course <sebastian.alvarado@mail.utoronto.ca>
2016-06-10 13:04:09 -0500

    Added authentication methods for ensuring a user is logged in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,25 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  #Let Rails know that the following are helper methods. This will make it available to the views.
+  helper_method :current_user, :logged_in?
+
+  def current_user
+  	#Returns the user if the current session already exists, if not find it 
+  	@current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def logged_in?
+  	#Convert the current_user to a boolean and see if we have anyone logged in
+  	!!current_user
+  end
+
+  def require_user
+  	#Check if there is a logged in user or no
+  	if !logged_in?
+  		flash[:danger] = "You must be logged in to perform that action"
+  		redirect_to root_path
+  	end
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
 
+	before_action :set_user, only: [:edit, :update, :show]
 	def new
 		@user = User.new
 	end
@@ -21,11 +22,10 @@ class UsersController < ApplicationController
 
 	def edit 
 		#find user ID
-		@user = User.find(params[:id])
+
 	end
 
 	def update
-		@user = User.find(params[:id])
 
 		if @user.update(user_params)
 			flash[:success] = "Your account was updated successfully!"
@@ -38,11 +38,15 @@ class UsersController < ApplicationController
 
 	def show
 		#Find the user to show
-		@user = User.find(params[:id])
 		@user_articles = @user.articles.paginate(page: params[:page], per_page: 5)
 	end
 	private 
 	def user_params
 		params.require(:user).permit(:username, :email, :password)
+	end
+
+	#Have the @user instance variable ready to use before edit, update and show
+	def set_user
+		@user = User.find(params[:id])
 	end
 end
